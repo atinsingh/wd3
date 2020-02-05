12e6bfa721252d62b3fc7b2dd798a98d2e5083a4
Singh,Atin <atin@pragra.co>
2020-02-05 11:59:54 -0500

    Added First Version

---
+++ firstproject-05022020/src/FirstClass.java
@@ -0,0 +1,17 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        int populationOfClass ;
+        // variable in java is delacared following way
+        // type <name of variables >
+        short shortIntVariable  = 32767;
+
+        // short takes 16 bit or 2 byte
+        long worldsPopulation = 7_500_000_000L;
+
+        System.out.println(worldsPopulation);
+
+        /** put 3 short, 3 long , 3 int
+         * assign value and print them
+         */
+    }
+}
